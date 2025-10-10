87761769a812eb491c19474ea4ea45db1bd3faa7
IntrospectiveGorilla <annabeledmonston@gmail.com>
2025-10-09 21:45:05 -0400

    Added a lil fisher sprite (temporary)

--- core/src/main/java/io/github/javagame/GameScreen.java
+++ core/src/main/java/io/github/javagame/GameScreen.java
@@ -7,13 +7,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Gdx;
 
+
+//https://alohaeee.itch.io/fishing-assets for the fishin lady
+
 public class GameScreen implements Screen {
     final FishinGame game;
     private Texture gameBg;
+    private Texture fisher;
 
     public GameScreen(final FishinGame game) {
         this.game = game;
         gameBg = new Texture("kiddiepool.jpg");
+        fisher = new Texture("Fisher000.png");
     }
 
     @Override
@@ -27,6 +32,7 @@ public class GameScreen implements Screen {
         game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
         game.batch.begin();
         game.batch.draw(gameBg, 0, 0,game.viewport.getWorldWidth(),game.viewport.getWorldHeight());
+        game.batch.draw(fisher,10,10,(game.viewport.getWorldWidth()/6),(game.viewport.getWorldHeight()/6));
         //keeps camera units and drawing units consistent, while Gdx.graphics uses pixels instead or something. It's weird
         //game.batch.draw(gameBg, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         game.batch.end();
