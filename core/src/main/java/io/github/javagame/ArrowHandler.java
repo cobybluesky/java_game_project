164b2f2e14a9dc0d7a545f489ad39987b1ad4e34
Caleb Delagrave <cobyblue98@gmail.com>
2025-10-14 11:13:16 -0400

    params for arrow spacing, speed, and sequence length

--- core/src/main/java/io/github/javagame/ArrowHandler.java
+++ core/src/main/java/io/github/javagame/ArrowHandler.java
@@ -15,8 +15,13 @@ public class ArrowHandler {
         new Sprite(new Texture("colour_right.png"))
     };
     private float[] arrowColumns = {7, 10, 13, 16};
-    public float arrowSpeed = 4.0f;
-    public float arrowDelay = 0.5f;// time between arrows
+    private float arrowSpeed;
+    private float arrowDelay;// time between arrows
+    private float arrowTimer;
+    private float arrowChance = 0.8f;// chance an arrow actually spawns (possibility of blank rows)
+    private int sequenceRows; // number of rows (w or w/o arrows) in the sequence
+    private int rowCount;
+    private boolean spawnArrows = false;
 
     static Random random = new Random();
 
@@ -46,7 +51,18 @@ public class ArrowHandler {
         visibleArrows.add(newArrow);
     }
 
-    public void moveArrows(float delta) {
+    public void updateArrows(float delta) {
+        // eventually add a chance for multiple arrows on one line
+        // currently random spawning
+        if (spawnArrows) {
+            arrowTimer -= delta;
+            if (arrowTimer <= 0) {
+                // random chance for arrow not to appear (guaranteed on first arrow)
+                if (Math.random() < arrowChance || rowCount == sequenceRows) { createArrow(); }
+                arrowTimer = arrowDelay;
+                rowCount += 1;
+            }
+        }
         // iterate backwards so removing doesn't mess it up
         for (int i = visibleArrows.size() - 1; i >= 0; i--) {
             Sprite arrow = visibleArrows.get(i);
@@ -57,11 +73,23 @@ public class ArrowHandler {
                 // player did not click in time
             }
         }
+        if (rowCount >= sequenceRows) {
+            // end sequence
+            spawnArrows = false;
+        }
     }
 
     public void drawArrows() {
         for (Sprite arrow : visibleArrows) {
             arrow.draw(game.batch);
         }
+    }
+
+    public void beginArrowSequence(float speed, float delay, int sequenceLen) {
+        arrowDelay = delay;
+        arrowSpeed = speed;
+        sequenceRows = sequenceLen;
+        arrowTimer = arrowDelay;
+        spawnArrows = true;
     }
 }
